cee725948f67097547a744dc7205a95d5b08b4d9
Gabriela Sillis <gabydias47@gmail.com>
2021-11-29 12:46:08 -0300

    change script

--- .github/scripts/test.sh
+++ .github/scripts/test.sh
@@ -3,6 +3,6 @@
 set -eo pipefail
 
 xcodebuild -project To\ Do\ List.xcodeproj \
-            -scheme To Do List \
+            -scheme To\ Do\ List \
             -destination platform=iOS\ Simulator,OS=14.4,name=iPhone\ 11 \
             clean test | xcpretty
